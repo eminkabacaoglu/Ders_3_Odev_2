4c3104bb1c2acf9fca9d8e33afa363c644579e3a
eminkabacaoglu <eminkabacaoglu@gmail.com>
2022-10-07 01:50:28 +0300

    upload

---
+++ abstractClasses/src/abstractClasses/GameCalculator.java
@@ -0,0 +1,10 @@
+package abstractClasses;
+
+public abstract class GameCalculator {
+	
+	public abstract void calculate();
+	
+	public void gameOver() {
+		System.out.println("Game Over...");
+	}
+}
